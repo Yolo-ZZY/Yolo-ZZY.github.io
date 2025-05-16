d80af6260c507f72d5029f65fda25ccf7bf3dca4
yolo-zzy <2661572212@qq.com>
2025-05-16 23:24:09 +0800

    Site updated: 2025-05-16 23:23:27

---
+++ live2d-widget-master/src/types/svg.d.ts
@@ -0,0 +1,12 @@
+/**
+ * @file 定义 SVG 模块的类型。
+ * @module types/svg
+ */
+declare module '*.svg' {
+  /**
+   * SVG 内容。
+   * @type {string}
+   */
+  const content: string;
+  export default content;
+}
